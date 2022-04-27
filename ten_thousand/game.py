49e85c86ccaa68701b834db94af9d73a348a245e
Jacob Choi <jacob.j.choi@gmail.com>
2022-04-27 14:31:08 -0700

    added version 3 tests

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -26,6 +26,43 @@ class Game:
             elif response == "y":
                 self.start_round(roller)
 
+    def bank_earned_points(self, roller):
+        print(f"You banked {self.banker.shelved} points in round {self.rounds}")
+        self.banker.bank()
+        print(f"Total score is {self.banker.balance} points")
+        if self.banker.balance >=1000:
+            print("WINNER")
+        else:
+            self.start_round(roller)
+
+    # def zilch(self):
+    #     if len(GameLogic.get_scorers(self.rolled_dice)) == 0:
+    #         self.validate_dice()
+    #         return True
+
+        return False
+
+    def validate_dice(self, rolled_dice, roller):
+        if GameLogic.calculate_score(rolled_dice) == 0:
+            print("****************************************")
+            print("**        Zilch!!! Round over         **")
+            print("****************************************")
+            self.banker.clear_shelf()
+            self.bank_earned_points()
+        else:
+            print("Enter dice to keep or (q)uit:")
+            response = input(">")
+            if response:
+                response = response.replace(" ", "")
+            else: self.validate_dice(rolled_dice, roller)
+        if response == "q":
+            print(f"Thanks for playing. You earned {self.banker.balance} points")
+            sys.exit()
+        else: 
+            print("Cheater!!! Or possibly made a typo...")
+            return self.validate_dice(rolled_dice, roller)
+
+
     def start_round(self, roller):
 
         while self.status and self.banker.balance <= 1000:
@@ -33,7 +70,10 @@ class Game:
             print(f"Starting round {self.rounds}")
             print(f"Rolling {self.dice_quantity} dice...")
             roll = roller(self.dice_quantity)
-            roller_str = " ".join(map(str, roll))
+            # roller_str = " ".join(map(str, roll))
+            roller_str = ""
+            for num in roll:
+                roller_str += str(num) + " "
             print(f"*** {roller_str} ***")
             print(f"Enter dice to keep, or (q)uit:")
             response = input("> ")
@@ -43,7 +83,6 @@ class Game:
                 sys.exit()
             else:
                 user_response = tuple(map(int, list(response)))
-
                 self.dice_quantity -= len(user_response)
                 score = GameLogic.calculate_score(user_response)
                 self.banker.shelf(score)
